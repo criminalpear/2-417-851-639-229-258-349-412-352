0107a7b05e3a38baf597993d6fe28a9c4b56c01a
CriminalPear <evretcasavan@gmail.com>
2025-09-17 06:47:51 -0500

    Delete js directory

--- js/application.js
+++
@@ -1,6 +0,0 @@
-var GM;
-
-// Wait till the browser is ready to render the game (avoids glitches)
-window.requestAnimationFrame(function () {
-  GM = new GameManager(9, KeyboardInputManager, HTMLActuator, LocalStorageManager);
-});
