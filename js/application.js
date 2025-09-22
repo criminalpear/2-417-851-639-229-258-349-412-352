6a977af663edc05be03a33ba4ad363e363c7421a
CriminalPear <evretcasavan@gmail.com>
2025-09-21 20:37:36 -0500

    Delete js directory

--- js/application.js
+++
@@ -1,6 +0,0 @@
-// Ensure GM is globally accessible and initialized once
-window.GM = null;
-
-window.requestAnimationFrame(function () {
-  window.GM = new GameManager(9, KeyboardInputManager, HTMLActuator, LocalStorageManager);
-});
